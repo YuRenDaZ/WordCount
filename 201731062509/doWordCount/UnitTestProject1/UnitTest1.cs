3e774d60028a3c7ca472f79fec0ed9714f67cca5
softwareXS <1350220325@qq.com>
2019-04-01 19:25:36 +0800

    第三次作业第二次提交

---
+++ 201731062509/doWordCount/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using doWordCount;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()//测试doCount.CountLine计算文章总行数
+        {
+            DoCount doCount = new DoCount();
+            int line = doCount.CountLine("e:\\Text.txt");
+            Console.WriteLine(line);
+        }
+        [TestMethod]
+        public void TestMethod2()//测试doCount.CountFrequency计算每个单词频数
+        {
+            DoCount doCount = new DoCount();
+            Dictionary<string,int> dictionary=doCount.CountFrequency("e:\\Text1.txt");
+            dictionary=doCount.SortDictionary_Desc(dictionary);
+             foreach (KeyValuePair<string,int> dic in dictionary)
+             {
+                Console.WriteLine("单词：" + dic.Key + "\t         " + "\t频数：" + dic.Value);
+            }
+        }
+        [TestMethod]
+        public void TestMethod3()//测试doCount.CountCahr计算文章总的字母个数
+        {
+            DoCount doCount = new DoCount();
+            Console.WriteLine(doCount.CountChar("e:\\Text1.txt"));
+        }
+        [TestMethod]
+        public void TestMethod4()//测试doCount.CountCahr计算文章总的字母个数
+        {
+            DoCount doCount = new DoCount();
+            doCount.WriteToTxt("e:\\Text1.txt","e:\\output.txt");
+        }
+    }
+}
---
+++ 201731062509/doWordCount/doWordCount/count.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace doWordCount
+{
+    public interface Count {
+        int CountChar(string path);//计算总字符个数
+
+        int CountWord(string path);//计算文本中总单词数
+
+        int CountLine(string path);//计算文本总行数
+
+        Dictionary<string,int> CountFrequency(string path);//计算文本中每个单词的频数
+
+        Dictionary<string, int> SortDictionary_Desc(Dictionary<string, int> dic);//将字母按频数降序排序
+
+        void WriteToTxt(string path,string outPath);
+    };
+}
