afe988808007ec68da3dcf096889a338329fbdf2
justice1933 <1933424281@qq.com>
2019-04-02 20:31:23 +0800

    第三次作业

---
+++ 201731062507/doWordCount/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using doWordCount;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()//测试doCount.CountLine计算文章总行数
+        {
+            DoCount doCount = new DoCount();
+            int line = doCount.CountLine("C:\\Text.txt");
+            Console.WriteLine(line);
+        }
+        [TestMethod]
+        public void TestMethod2()//测试doCount.CountFrequency计算每个单词频数
+        {
+            DoCount doCount = new DoCount();
+            Dictionary<string,int> dictionary=doCount.CountF("c:\\Text.txt");
+            dictionary=doCount.DescSort(dictionary);
+             foreach (KeyValuePair<string,int> dic in dictionary)
+             {
+                 Console.WriteLine(dic.Key+" "+dic.Value );
+             }
+        }
+        [TestMethod]
+        public void TestMethod3()//测试doCount.CountCahr计算文章总的字母个数
+        {
+            DoCount doCount = new DoCount();
+            Console.WriteLine(doCount.CountChar("c:\\Text.txt"));
+        }
+        [TestMethod]
+        public void TestMethod4()//测试doCount.CountCahr计算文章总的字母个数
+        {
+            DoCount doCount = new DoCount();
+            doCount.Write("c:\\Text.txt", "c:\\output.txt");
+        }
+    }
+}
---
+++ 201731062507/doWordCount/doWordCount/doWordCount/mainClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWordCount
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Console.WriteLine();
+        }
+    }
+}
