fbc8aa7e9825ca33515fa85f88073c2afd415f55
975153251 <975153251@qq.com>
2019-04-03 20:27:04 +0800

    WordCount

---
+++ 201731062428/WordCount/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("77bcd113-968b-41d4-b57e-a9a45bf2f01c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 201731062428/WordCount/UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using wordCount;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FileInfo file = new FileInfo(@"D:\input.txt");
+            StreamReader sw = file.OpenText();
+            List<string> test = new List<string>();
+            string str = sw.ReadToEnd();
+            int tlines = 6;
+            S s = new S();
+            int words = s.Countlines(str);
+            Assert.IsTrue(words == tlines);
+        }
+    }
+}
